859099f25ab3838c361b88004ce5778f7fce2d7a
sungmoonkimeu <sungmoon.kim@umons.ac.be>
2021-07-13 17:39:27 +0200

    eigen problem

--- FOCSsim_eigenstate.py
+++ FOCSsim_eigenstate.py
@@ -6,7 +6,7 @@ Created on Tue JUL 1 15:00:00 2021
 (Circular vessel shape)
 Spun fibre model with laming matrix
 """
-
+import cmath, math
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -23,6 +23,23 @@ import matplotlib.ticker
 from matplotlib.ticker import (MaxNLocator,
                                FormatStrFormatter, ScalarFormatter)
 
+def eigen_expm(A):
+    """
+
+    Parameters
+    ----------
+    A : 2 x 2 diagonalizable matrix
+        DESCRIPTION.
+
+    Returns
+    -------
+    expm(A): exponential of the matrix A.
+
+    """
+    vals, vects = eig(A)
+    return einsum('...ik, ...k, ...kj -> ...ij',
+                  vects, np.exp(vals), np.linalg.inv(vects))
+
 start = pd.Timestamp.now()
 
 cstm_color = ['c', 'm', 'y', 'k', 'r']
@@ -33,9 +50,9 @@ E = Jones_vector('Input pol')
 S = create_Stokes('Output_S')
 J = create_Jones_matrices('J')
 
+# Case 1
 V_azi = np.hstack((pi/100, arange(0, pi/4, pi/16), pi/4))
 V_mod_ret = arange(0, -pi/2, -pi/100)
-
 E.linear_light(azimuth=V_azi)
 J.retarder_linear(R=V_mod_ret)
 for nn in range(len(V_azi)):
@@ -46,12 +63,12 @@ for nn in range(len(V_azi)):
     else:
         draw_stokes_points(fig[0], S, kind='line', color_line='b')
 
+# Case 2
 V_ell = np.hstack((arange(0, -pi/4, -pi/16), -pi/4+pi/100))
-# ellipticity 0 ~ pi/4 -->
+# ellipticity 0 ~ pi/4 --> linear to circular
 V_mod_rot = arange(0, pi/8, pi/100)
-
 E.general_azimuth_ellipticity(azimuth=0, ellipticity=V_ell)
-J.half_waveplate(azimuth=V_mod_rot, length=len(V_mod_rot))
+J.half_waveplate(azimuth=V_mod_rot)
 for nn in range(len(V_ell)):
     V_out = J*E[nn]
     S.from_Jones(V_out)
@@ -60,4 +77,51 @@ for nn in range(len(V_ell)):
     else:
         draw_stokes_points(fig[0], S, kind='line', color_line='b')
 
+# Case 3
+J2 = create_Jones_matrices('J2')
+
+theta = np.arange(0, 90, 15)
+A = np.array([[cos(theta*pi/180), -sin(theta*pi/180)], [sin(theta*pi/180), cos(theta*pi/180)]])
+J2.from_matrix(A)
+
+a, b = J2.parameters.eigenvalues()
+phase_a = zeros(len(a))
+phase_b = zeros(len(b))
+
+for nn, vv in enumerate(a):
+    phase_a[nn] = math.degrees(cmath.phase(vv))
+for nn, vv in enumerate(b):
+    phase_b[nn] = math.degrees(cmath.phase(vv))
+
+print(phase_a)
+print(phase_b)
+
+c, d = J2.parameters.eigenstates()
+e = np.empty((len(c), 2, 2))*1j
+for nn in range(len(c)):
+    e[nn] = np.hstack((c[nn].parameters.matrix(), d[nn].parameters.matrix()))
+
+print(e)
+
+
+for nn in range(len(c)):
+    phi_a = phase_a[nn] * pi / 180
+    m_a = np.array([[cos(phi_a), -sin(phi_a)], [sin(phi_a), cos(phi_a)]])
+    phi_b = phase_b[nn]*pi/180
+    m_b = np.array([[cos(phi_b), -sin(phi_b)], [sin(phi_b), cos(phi_b)]])
+
+    print(m_b@e[nn]@m_a)
+
+E.linear_light(azimuth=-45*pi/180)
+J.half_waveplate(azimuth=30*pi/180)
+
+V_mod_ret = arange(0, -pi/2, -pi/100)
+
+
+J2.retarder_linear(R=V_mod_ret)
+V_out = J2*J*E
+S.from_Jones(V_out)
+fig, ax = S.draw_poincare(figsize=(7, 7), angle_view=[33 * pi / 180, 53 * pi / 180], kind='line', color_line='b')
+
 plt.show()
+
