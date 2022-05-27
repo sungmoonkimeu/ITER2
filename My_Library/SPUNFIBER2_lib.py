1ea1c2bb30858076201af327951547aca4cfe38f
sungmoonkimeu <sungmoon.kim@umons.ac.be>
2022-05-27 17:25:00 +0200

    SPUNFIBRE_lib.py 이상함 언능 고쳐라

--- My_Library/SPUNFIBER2_lib.py
+++ My_Library/SPUNFIBER2_lib.py
@@ -1471,10 +1471,9 @@ if __name__ == '__main__':
         # ax[2].set_xticklabels(str_xtick, minor=False, rotation=-45)
 
     if mode == 5:
-        L = 0.5
-        dz =0.5
-
-        LB = 10000000
+        L = 1
+        dz = 1
+        LB = 0.009
         SP = 0.005
 
         V = 0.54 * 4 * pi * 1e-7
@@ -1488,9 +1487,9 @@ if __name__ == '__main__':
         n2 = 0
         m2 = 0
 
-        vV_I = [3e6, 4e6, 5e6]
+        vV_I = [1e6, 4e6, 5e6]
 
-        H = vV_I[1] / L/2
+        H = vV_I[1] / L
         rho = V * H
 
         # --------Laming: orientation of the local slow axis ------------
@@ -1508,9 +1507,10 @@ if __name__ == '__main__':
         R_zf = 2 * arcsin(sin(gma * dz) / ((1 + qu ** 2) ** 0.5))
 
         Le = 2 * pi / gma
-        V_nf = -((V_z / (Le / 4)).astype(int) / 2).astype(int)
+        #V_nf = -((V_z / (Le / 4)).astype(int) / 2).astype(int)
+        nf = -int(gma*dz/pi)-1 if arctan((-qu / ((1 + qu ** 2) ** 0.5)) * tan(gma * dz)) > 0 else -int(gma*dz/pi)
 
-        Omega_zf = s_t_r * dz + arctan((-qu / ((1 + qu ** 2) ** 0.5)) * tan(gma * dz)) + V_nf[1] * pi
+        Omega_zf = s_t_r * dz + arctan((-qu / ((1 + qu ** 2) ** 0.5)) * tan(gma * dz)) + nf * pi
         Phi_zf = ((s_t_r * dz) - Omega_zf) / 2 + m * (pi / 2)
 
         # forward
@@ -1529,5 +1529,44 @@ if __name__ == '__main__':
             M_Omega_f = np.array([[cos(Omega_z2), -sin(Omega_z2)], [sin(Omega_z2), cos(Omega_z2)]])
             MF = M_Omega_f @ M_R_f @ MF
 
-        print(MF)
+        # define backward
+        # spin and farday rotation.
+        s_t_r = -2 * pi / SP  # spin twist ratio
+        V_theta_1s = V_z * s_t_r
+        qu = 2 * (s_t_r - rho) / delta
+        gma = 0.5 * (delta ** 2 + 4 * ((s_t_r - rho) ** 2)) ** 0.5
+
+        R_zb = 2 * arcsin(sin(gma * dz) / ((1 + qu ** 2) ** 0.5))
+        nb = int(gma * dz / pi) if arctan((-qu / ((1 + qu ** 2) ** 0.5)) * tan(gma * dz)) > 0 else int(
+            gma * dz / pi)+1
+
+        Omega_zb = s_t_r * dz + arctan((-qu / ((1 + qu ** 2) ** 0.5)) * tan(gma * dz)) + nb * pi
+        Phi_zb = ((s_t_r * dz) - Omega_zf) / 2 + m * (pi / 2)
+
+        # backward
+        MB = np.array([[1, 0], [0, 1]])
+        for kk in range(len(V_theta_1s) - 1):
+            Omega_z2 = Omega_zb
+            Phi_z2 = Phi_zb + V_theta_1s[-1-kk]
+            print("Omega[", kk, "]:", Omega_z2)
+            print("Phi[", kk, "]:", Phi_z2)
+            R_z2 = R_zb
+            n11 = cos(R_z2 / 2) + 1j * sin(R_z2 / 2) * cos(2 * Phi_z2)
+            n12 = 1j * sin(R_z2 / 2) * sin(2 * Phi_z2)
+            n21 = 1j * sin(R_z2 / 2) * sin(2 * Phi_z2)
+            n22 = cos(R_z2 / 2) - 1j * sin(R_z2 / 2) * cos(2 * Phi_z2)
+            M_R_b = np.array([[n11, n12], [n21, n22]])
+            M_Omega_b = np.array([[cos(Omega_z2), -sin(Omega_z2)], [sin(Omega_z2), cos(Omega_z2)]])
+            MB = M_Omega_b @ M_R_b @ MB
+
+        ksi =45
+        Rot = np.array([[cos(ksi), -sin(ksi)], [sin(ksi), cos(ksi)]])
+        Jm = np.array([[1, 0], [0, 1]])
+        M_FR = Rot @ Jm @ Rot
+
+        V_in = np.array([[1],[0]])
+
+        V_out = MB@M_FR@MF@V_in
+        print(V_out)
+
 plt.show()
